8f4e379e13dbbf566c618131a97cde7fcc9cfa22
Wojciech Czerpak <wojciech.czerpak+gitlab@gmail.com>
2015-07-01 15:30:03 +0200

    fragment event add layout is almost ready. 1. dashed line dont want to draw and checkbox is different then in mockup.

--- Let'sMeet/app/src/main/java/com/example/letsmeet/letsmeet/Activities/ActivityMain.java
+++ Let'sMeet/app/src/main/java/com/example/letsmeet/letsmeet/Activities/ActivityMain.java
@@ -52,7 +52,7 @@ public class ActivityMain extends AppCompatActivity implements NavigationView.On
         setUpNavDrawer();
 
         // test
-        openFragment(FRAGMENT_REGISTER);
+        openFragment(FRAGMENT_EVENT_ADD);
     }
 
     private void setUpNavDrawer() {
--- Let'sMeet/app/src/main/java/com/example/letsmeet/letsmeet/Fragments/FragmentEventAdd.java
+++ Let'sMeet/app/src/main/java/com/example/letsmeet/letsmeet/Fragments/FragmentEventAdd.java
@@ -1,15 +1,47 @@
 package com.example.letsmeet.letsmeet.Fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.letsmeet.letsmeet.R;
 
 /**
  * Created by Soot_Win on 2015-03-21.
  */
 public class FragmentEventAdd extends Fragment {
 
+    // view's
+    private EditText eName;
+    private EditText eCategory;
+    private EditText eDate;
+    private EditText ePlace;
+    private EditText eAddress;
+    private EditText eParticipantsNumber;
+    private Button bAdd;
+
     public static Fragment newInstance (){
         FragmentEventAdd f = new FragmentEventAdd();
         return f;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.fragment_event_add, container, false);
+
+        setUpViews(layout);
+
+        return layout;
+    }
+
+    private void setUpViews(View layout) {
+//        eName       = (EditText) layout.findViewById(R.id.fragmentEventAddName);
     }
 
 
